b8d5c25be14d723bab745d5242dc66e4428d3210
Anna Shevchenko <uaskoa@gmail.com>
2021-06-14 22:47:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import Statistics from './components/Statistics';
 import FeedbackOptions from './components/FeedbackOptions/';
 import Section from './components/Section';
 import Notification from './components/Notification';
 import './App.scss'
+
+// export default function App () {
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+
+//   const countTotalFeedback = () => {
+//     return good + neutral + bad;
+//   };
+
+//   const countPositiveFeedbackPercentage = () => {
+//     return Math.round((good * 100) / countTotalFeedback);
+//   };
+
+// ;
+
+//    const handleFeedback = (option) => {
+//      console.log(evt)
+//     //  const { name } = evt.target;
+//     //  switch (name) {
+//     //    case "good":
+//     //      setGood((prevState) => prevState + 1);
+//     //      break;
+
+//     //    case "neutral":
+//     //      setNeutral((prevState) => prevState + 1);
+//     //      break;
+
+//     //    case "bad":
+//     //      setBad((prevState) => prevState + 1);
+//     //      break;
+
+//     //    default:
+//     //      break;
+//     //  }
+//   }
+
+
+ 
+//     return (
+//       <div className="container">
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={[good, neutral, bad]}
+//             onLeaveFeedback={handleFeedback}
+//           />
+//         </Section>
+
+//         {countTotalFeedback() ? (
+//           <Section title="Statistics">
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={countTotalFeedback}
+//               positivePercentage={countPositiveFeedbackPercentage}
+//             />
+//           </Section>
+//         ) : (
+//           <Notification message="No feedback given"></Notification>
+//         )}
+//       </div>
+//     );
+  
+// }
+
+
+
 
 class App extends Component {
   state = {
@@ -26,6 +95,8 @@ class App extends Component {
   };
 
   render() {
+
+    console.log(this.state);
     return (
       <div className='container'>
         <Section title="Please leave feedback">
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -14,7 +14,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => (
 
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  options: PropTypes.array.isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 }
 
